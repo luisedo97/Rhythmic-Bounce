198f1113553a9bf74be236a269c031e9c64fa236
Luis E. Araujo B <luiseduardoab97@users.noreply.github.com>
2019-02-01 11:40:52 -0400

    Logic Cubes Refactor

---
+++ Assets/ControllerCubes.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCubes : MonoBehaviour
+{
+
+    public Cube[] cubes;
+    [Range(1, 50)]
+    public float upCubeSpeed = 16;
+    private float height = 0f;
+    private int counter = 0;
+    [Range(0, 100)]
+    public float speedRotate = 41.5f;
+
+    void Awake()
+    {
+        height = cubes[cubes.Length - 1].transform.position.y + 0.5f;
+    }
+
+    void Update()
+    {
+        upCube();
+        transform.Rotate(Vector3.up * -speedRotate * Time.deltaTime);
+    }
+
+
+    void upCube()
+    {
+        if (cubes[counter].up(height, upCubeSpeed))
+        {
+            height += 0.5f;
+            //Debug.Log("Diferencia:" + (sun.transform.position.y - cubes[counter].gameObject.transform.position.y));
+            if (counter + 1 == cubes.Length)
+            {
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+        }
+    }
+
+    //Debuger
+    IEnumerator Debuger()
+    {
+        while (true)
+        {
+            float counterPast = counter;
+            yield return new WaitForSecondsRealtime(1f);
+            //Debug.Log("Cubos movidos:" + (counter - counterPast));
+        }
+    }
+}
--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -5,55 +5,21 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public GameObject sun;
-    public Cube[] cubes;
-    [Range(0, 100)]
-    public float speedRotate = 41.5f;
-    [Range(1, 50)]
-    public int upCubeSpeed = 16;
-    private float height = 0f;
+    
+    
     public float speedGlobal = 1f;
-    private int counter = 0;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        height = cubes[cubes.Length-1].transform.position.y + 0.5f;
-        //StartCoroutine(upCube());
-        StartCoroutine(Counter());
-    }
 
-    IEnumerator Counter()
-    {
-        while (true)
-        {
-            float counterPast = counter;
-            yield return new WaitForSecondsRealtime(1f);
-            //Debug.Log("Cubos movidos:" + (counter - counterPast));
-        }
-    }
-
-    void upCube() {
-
-        if (cubes[counter].up(height, upCubeSpeed))
-        {
-            height += 0.5f;
-            Debug.Log("Diferencia:" + (sun.transform.position.y - cubes[counter].gameObject.transform.position.y));
-            if (counter + 1 == cubes.Length)
-            {
-                counter = 0;
-            }
-            else
-            {
-                counter++;
-            }
-        }
     }
 
     private void Update()
     {
-        upCube();
+        //upCube();
         transform.position = new Vector3(transform.position.x, sun.transform.position.y, transform.position.z);
-        GameObject.Find("Cubes").transform.Rotate(Vector3.up*-speedRotate*Time.deltaTime);
+        
     }
 
 
